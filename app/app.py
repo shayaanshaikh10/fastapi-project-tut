b5c1dda7ae909128da26597e2eac725e0eb062ac
shayaanshaikh10 <shayaanshaikhlm10@gmail.com>
2026-09-25 01:04:30 +0530

    feat: add file upload endpoint and feed retrieval functionality

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI,HTTPException, File, UploadFile, Form, Depends 
 from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
@@ -10,4 +11,27 @@ async def lifespan(app:FastAPI):
     yield
 
 app=FastAPI(lifespan=lifespan) #created the fast api application
+
+@app.post("/upload")
+async def  upload_file(
+    file:upload_file=File(...),
+    caption:str=Form(""),
+    session:AsyncSession=Depends(get_async_session)
+):
+    post=Post(
+        caption=caption,
+        url="dummyurl",
+        file_type="photo",
+        file_name="dummy name  "
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
+
+@app.get("/feed")
+async def get_feed(
+    session:AsyncSession=Depends(get_async_session)
+):
+    await session.execute(select(Post).order_by(Post.created_at.desc()))
 
